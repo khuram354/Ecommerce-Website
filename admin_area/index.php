3b06be02eca6348caac4728a32e42b3ac64bbb37
Khuram Shahzad <khurammirza354@gmail.com>
2023-10-04 12:28:10 +0500

    insert categories and brands

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,6 +8,10 @@
     <!-- Bootstrap CSS Link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Font Awesome Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Custom CSS File Link -->
     <link rel="stylesheet" href="../style.css">
 
@@ -56,28 +60,49 @@
                     <p class="text-light text-center">Admin Name</p>
                 </div>
                 <div class="button text-center">
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Insert
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Insert
                             Products</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">View
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">View
                             Product</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Insert
+                    <button class="my-3 border-0"><a href="index.php?insert_category"
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Insert
                             Categories</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">View
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">View
                             Categories</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Insert
+                    <button class="my-3 border-0"><a href="index.php?insert_brand"
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Insert
                             Brands</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">View
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">View
                             Brands</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">All
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">All
                             Orders</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">All
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">All
                             Payments</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">List
+                    <button class="my-3 border-0"><a href=""
+                            class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">List
                             Users</a></button>
-                    <button class="my-3"><a href=""
+                    <button class="my-3 border-0"><a href=""
                             class="nav-link text-dark fw-bold bg-info m-1 px-2 py-1">Logout</a></button>
                 </div>
             </div>
+        </div>
+
+        <!-- 4th child -->
+        <div class="container my-5">
+            <?php
+            if (isset($_GET['insert_category'])) {
+                include('insert_categories.php');
+            }
+            if (isset($_GET['insert_brand'])) {
+                include('insert_brands.php');
+            }
+            ?>
         </div>
 
         <!-- last child -->
