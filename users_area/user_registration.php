ffa4309d83ea0925c40fdbc982984bc158e5dc54
Khuram Shahzad <khurammirza354@gmail.com>
2023-10-09 21:25:52 +0500

    user exists or not logic added

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -164,18 +164,26 @@ if (isset($_POST['user_register'])) {
     $user_image_tmp = $_FILES['user_image']['tmp_name'];
     $user_ip = getIPAddress();
 
-    //insert query
-    move_uploaded_file($user_image_tmp, "./user_images/$user_image");
-    $insert_query = "INSERT INTO `user_table` (username,user_email,user_password,user_image,user_ip,user_address,user_mobile) VALUES ('$user_username','$user_email','$user_password','$user_image','$user_ip','$user_address','$user_contact')";
-    $sql_execute = mysqli_query($con, $insert_query);
+    //select query
+    $select_query = "SELECT * FROM `user_table` WHERE username='$user_username'";
+    $result = mysqli_query($con, $select_query);
+    $rows_count = mysqli_num_rows($result);
+    if ($rows_count > 0) {
+        echo "<script>alert('Username already exists. Please choose another username!')</script>";
+    } else if ($user_password != $conf_user_password) {
+        echo "<script>alert('Password and Confirm Password are not matched.')</script>";
+    } else {
+        //insert query
+        move_uploaded_file($user_image_tmp, "./user_images/$user_image");
+        $insert_query = "INSERT INTO `user_table` (username,user_email,user_password,user_image,user_ip,user_address,user_mobile) VALUES ('$user_username','$user_email','$user_password','$user_image','$user_ip','$user_address','$user_contact')";
+        $sql_execute = mysqli_query($con, $insert_query);
+    }
     if ($sql_execute) {
         echo "<script>alert('Data inserted Successfully!')</script>";
     } else {
         die('Connection failed: ' . mysqli_connect_error());
     }
 
-
 }
-
 
 ?>
