0d222be435de4264251e8103bc54e835d817e9dd
Khuram Shahzad <khurammirza354@gmail.com>
2023-10-11 00:55:43 +0500

    payment page working

--- users_area/payment.php
+++ users_area/payment.php
@@ -1,14 +1,50 @@
+<?php
+include('../includes/connect.php');
+include('../functions/common_function.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Payment Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <style>
+        img {
+            width: 90%;
+            /* margin: auto;
+            display: block; */
+        }
+    </style>
 </head>
 
 <body>
-    <h1>payment</h1>
+    <!-- php code to access user id -->
+    <?php
+    $user_ip = getIPAddress();
+    $select_user = "SELECT * FROM `user_table` WHERE user_ip = '$user_ip'";
+    $result = mysqli_query($con, $select_user);
+    $run_query = mysqli_fetch_array($result);
+    $user_id = $run_query['user_id'];
+
+    ?>
+
+    <div class="container">
+        <h2 class="text-center text-info">Payment Options</h2>
+        <div class="row d-flex justify-content-center align-items-center my-5">
+            <div class="col-md-6">
+                <a href="https://www.paypal.com" target="_blank"><img src="../images/upi.jpg" alt="payment methods"></a>
+            </div>
+            <div class="col-md-6">
+                <a href="order.php?user_id=<?php echo $user_id ?>">
+                    <h2 class="text-center">Pay Offline</h2>
+                </a>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
