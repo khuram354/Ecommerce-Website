d71be413190ee00cab5b89c942f4b0a7f36f8132
Khuram Shahzad <khurammirza354@gmail.com>
2023-10-11 10:31:51 +0500

    inserting orders into database

--- users_area/order.php
+++ users_area/order.php
@@ -7,23 +7,43 @@ if (isset($_GET['user_id'])) {
 
 }
 
-
-//getting total items and total price of all items in cart
+//getting total price of all items in cart
 $get_ip_address = getIPAddress();
 $total_price = 0;
 $cart_query_price = "SELECT * FROM `cart_details` WHERE ip_address = '$get_ip_address'";
 $result_cart_price = mysqli_query($con, $cart_query_price);
-$cound_products = mysqli_num_rows($result_cart_price);
+$invoice_number = mt_rand();
+$status = 'pending';
+$count_products = mysqli_num_rows($result_cart_price);
 while ($row_price = mysqli_fetch_array($result_cart_price)) {
     $product_id = $row_price['product_id'];
     $select_product = "SELECT * FROM `products` WHERE product_id = $product_id";
     $run_price = mysqli_query($con, $select_product);
     while ($row_product_price = mysqli_fetch_array($run_price)) {
         $product_price = array($row_product_price['product_price']);
-        $product_values = array_sum($rowproduct_price);
+        $product_values = array_sum($row_product_price);
         $total_price += $product_values;
     }
 }
 
+//getting quantity of items in cart
+$select_cart = "SELECT * FROM `cart_details`";
+$run_cart = mysqli_query($con, $select_cart);
+$get_item_quantity = mysqli_fetch_array($run_cart);
+$quantity = $get_item_quantity['quantity'];
+if ($quantity == 0) {
+    $quantity = 1;
+    $subtotal = $total_price;
+} else {
+    $quantity = $quantity;
+    $subtotal = $total_price * $quantity;
+}
+
+$insert_orders = "INSERT INTO `user_orders` (user_id,amount_due,invoice_number,total_products,order_date,order_status) VALUES ($user_id,$subtotal,$invoice_number,$count_products,NOW(),'$status')";
+$result_query = mysqli_query($con, $insert_orders);
+if ($result_query) {
+    echo "<script>alert('Orders have been submitted successfully!')</script>";
+    echo "<script>window.open('profile.php','_self')</script>";
+}
 
 ?>
