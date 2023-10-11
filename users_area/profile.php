a64cc045af40f7216d393098c9b6dfeb0f273970
Khuram Shahzad <khurammirza354@gmail.com>
2023-10-11 16:13:45 +0500

    working on profile page

---
+++ users_area/profile.php
@@ -0,0 +1,159 @@
+<!-- connect file -->
+<?php
+include('../includes/connect.php');
+include('../functions/common_function.php');
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome
+        <?php echo $_SESSION['username'] ?>
+    </title>
+    <!-- Bootstrap CSS Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Font Awesome Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Custom CSS File Link -->
+    <link rel="stylesheet" href="../style.css">
+
+</head>
+
+<body>
+    <!-- navbar starts -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="../images/khuram_logo.svg" alt="our logo" class="pb-3">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../display_all.php">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile.php">My Account</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../cart.php"><i class="fa-solid fa-cart-shopping"></i><sup>
+                                    <?php cart_item(); ?>
+                                </sup></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Total Price:
+                                <?php total_cart_price(); ?>/-
+                            </a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" action="../search_product.php" method="get">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+                            name="search_data">
+                        <input type="submit" value="search" class="btn btn-outline-light" name="search_data_product">
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- calling cart function -->
+        <?php
+        cart();
+        ?>
+        <!-- 2nd child -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <ul class="navbar-nav me-auto">
+                <?php
+                if (!isset($_SESSION['username'])) {
+                    echo "<li class='nav-item'>
+                    <a class='nav-link' href='#'>Welcome Guest</a>
+                </li>";
+                } else {
+                    echo "<li class='nav-item'>
+                        <a class='nav-link' href='#'>Welcome " . $_SESSION['username'] . "</a>
+                        </li>";
+                }
+                if (!isset($_SESSION['username'])) {
+                    echo "<li class='nav-item'>
+                    <a class='nav-link' href='./users_area/user_login.php'>Login</a>
+                </li>";
+                } else {
+                    echo "<li class='nav-item'>
+                    <a class='nav-link' href='./users_area/logout.php'>Logout</a>
+                </li>";
+                }
+                ?>
+            </ul>
+        </nav>
+        <!-- 3rd child -->
+        <div class="bg-light">
+            <h3 class="text-center">Our Store</h3>
+            <p class="text-center">
+                Communication is the heart of community and e-commerce
+            </p>
+        </div>
+        <!-- 4th child -->
+        <div class="row">
+            <div class="col-md-2">
+                <ul class="navbar-nav bg-secondary text-center" style="height:100vh">
+                    <li class="nav-item bg-info">
+                        <a class="nav-link" href="#">
+                            <h4>Your Profile</h4>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <img src="./user_images/men_suit_300x300.jpg" alt="" class="profile_img">
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Pending Orders</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Edit Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">My Orders</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Delete Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-10">
+
+            </div>
+        </div>
+
+        <!-- last child -->
+        <!-- include footer -->
+        <?php
+        include('../includes/footer.php');
+        ?>
+    </div>
+
+    <!-- Bootstrap JS Link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
