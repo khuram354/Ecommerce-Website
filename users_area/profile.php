c060a278545eeafa0d65599306ef69d0b58ac9e2
Khuram Shahzad <khurammirza354@gmail.com>
2023-10-13 12:37:10 +0500

    user account delete logic added

--- users_area/profile.php
+++ users_area/profile.php
@@ -154,6 +154,9 @@ session_start();
                 if (isset($_GET['my_orders'])) {
                     include('user_orders.php');
                 }
+                if (isset($_GET['delete_account'])) {
+                    include('delete_account.php');
+                }
                 ?>
             </div>
         </div>
